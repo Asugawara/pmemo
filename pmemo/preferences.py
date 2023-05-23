4b2b1d68cd8041e494bdcbdc831ca3d027e2fe6d
Sugawara <asgasw@gmail.com>
2023-05-23 17:02:39 +0900

    add preference command to control pref(out_dir, theme, etc...) (#1)
    * add preference command to control pref(out_dir, theme, etc...)
    
    * fix toml file

---
+++ pmemo/preferences.py
@@ -0,0 +1,77 @@
+from enum import Enum
+from pathlib import Path
+
+from pydantic import BaseModel, PositiveInt
+
+
+class PygmentsStyles(str, Enum):
+    default = "default"
+    emacs = "emacs"
+    friendly = "friendly"
+    friendly_grayscale = "friendly_grayscale"
+    colorful = "colorful"
+    autumn = "autumn"
+    murphy = "murphy"
+    manni = "manni"
+    material = "material"
+    monokai = "monokai"
+    perldoc = "perldoc"
+    pastie = "pastie"
+    borland = "borland"
+    trac = "trac"
+    native = "native"
+    fruity = "fruity"
+    bw = "bw"
+    vim = "vim"
+    vs = "vs"
+    tango = "tango"
+    rrt = "rrt"
+    xcode = "xcode"
+    igor = "igor"
+    paraiso_light = "paraiso-light"
+    paraiso_dark = "paraiso-dark"
+    lovelace = "lovelace"
+    algol = "algol"
+    algol_nu = "algol_nu"
+    arduino = "arduino"
+    rainbow_dash = "rainbow_dash"
+    abap = "abap"
+    solarized_dark = "solarized-dark"
+    solarized_light = "solarized-light"
+    sas = "sas"
+    staroffice = "staroffice"
+    stata = "stata"
+    stata_light = "stata-light"
+    stata_dark = "stata-dark"
+    inkpot = "inkpot"
+    zenburn = "zenburn"
+    gruvbox_dark = "gruvbox-dark"
+    gruvbox_light = "gruvbox-light"
+    dracula = "dracula"
+    one_dark = "one-dark"
+    lilypond = "lilypond"
+    nord = "nord"
+    nord_darker = "nord-darker"
+    github_dark = "github-dark"
+
+
+class EditorPreference(BaseModel, frozen=True):
+    prompt_spaces: PositiveInt = 4
+    style_name: PygmentsStyles = PygmentsStyles.github_dark
+    indentation_spaces: PositiveInt = 4
+
+
+class MemoPreference(BaseModel, frozen=True):
+    max_title_length: PositiveInt = 30
+
+
+PREFERENCE_FILE_PATH = Path(__file__).parent / ".preference"
+
+
+class PmemoPreference(BaseModel, frozen=True):
+    out_dir: Path = Path.home() / ".pmemo"
+    memo_preference: MemoPreference = MemoPreference()
+    editor_preference: EditorPreference = EditorPreference()
+
+    def write(self):
+        PREFERENCE_FILE_PATH.write_text(self.json())
