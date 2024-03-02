8c05f88ad2cccd23ffc11c0097e10a3e7e19607f
Sugawara <asgasw@gmail.com>
2024-03-02 21:58:57 +0900

    error handling for 429 too many req (#19)

--- pmemo/api/client.py
+++ pmemo/api/client.py
@@ -29,6 +29,8 @@ class APIClient:
         )
         if res.status_code == requests.codes.ok:
             logger.info("Memo stored successfully: %s", file_name.decode("utf-8"))
+        elif res.status_code == requests.codes.too_many_requests:
+            logger.error("Too many requests")
         else:
             logger.error("Failed to store memo")
             if self._tokens.token:
@@ -39,6 +41,9 @@ class APIClient:
             self._config.memos,
             headers={"Authorization": f"Bearer {self._tokens.token}"},
         )
+        if res.status_code == requests.codes.too_many_requests:
+            logger.error("Too many requests")
+            return []
         if res.status_code != requests.codes.ok:
             logger.error("Failed to get memos")
             if self._tokens.token:
